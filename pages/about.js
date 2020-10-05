a4fa45be7b2f3df59c204ed4621a0459aa14c8ae
YuriySuro <suro1989@ukr.net>
2020-10-05 07:26:11 +0300

    reusable coponents done

---
+++ pages/about.js
@@ -0,0 +1,26 @@
+import React from "react";
+// const About = () => {
+//     let message = "Hello World";
+//     return (
+//         <h1>Hello About Page - {message}</h1>
+//     );
+// }
+    
+// export default About;
+
+/* const About = () => {
+    const message = "Hello World";
+    return React.createElement('h2', null, "I am generating this element with createElement method!");
+}
+
+export default About; */
+
+class About extends React.Component {
+    render() {
+        return (
+            <h1>Hello, I am class Component!</h1>
+        )
+    }
+}
+
+export default About;
