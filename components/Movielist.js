936fffe93a36b82f71a74ee8f09203a2c6ed34fa
YuriySuro <suro1989@ukr.net>
2020-10-06 22:02:20 +0300

    data code refactoring

--- components/Movielist.js
+++ components/Movielist.js
@@ -1,107 +1,39 @@
 import React from "react";
 
 class Movielist extends React.Component {
+
+    shorten = (text) => {
+        return text.substr(0, 60) + '...';
+    }
+
     render() {
+        const {movies} = this.props;
+    
         return (
             <>
-                <div className="col-lg-4 col-md-6 mb-4">
-                    <div className="card h-100">
-                        <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-                        <div className="card-body">
-                            <h4 className="card-title">
-                                <a href="#">Item One</a>
-                            </h4>
-                            <h5>$24.99</h5>
-                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                { movies.map(movie => {
+                    return (
+                        <div className="col-lg-4 col-md-6 mb-4" key={movie.id} >
+                            <div className="card h-100">
+                                <a href="#"><img className="card-img-top" src={movie.image} alt={movie.name} /></a>
+                                <div className="card-body">
+                                    <h4 className="card-title">
+                                        <a href="#">{movie.name}</a>
+                                    </h4>
+                                    <p className="card-text">{this.shorten(movie.description)}</p>
+                                </div>
+                                <div className="card-footer">
+                                    <small className="text-muted">{movie.rating}  &#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                                </div>
+                            </div>
                         </div>
-                        <div className="card-footer">
-                            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-lg-4 col-md-6 mb-4">
-                    <div className="card h-100">
-                        <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-                        <div className="card-body">
-                            <h4 className="card-title">
-                                <a href="#">Item Two</a>
-                            </h4>
-                            <h5>$24.99</h5>
-                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div className="card-footer">
-                            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-lg-4 col-md-6 mb-4">
-                    <div className="card h-100">
-                        <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-                        <div className="card-body">
-                            <h4 className="card-title">
-                                <a href="#">Item Three</a>
-                            </h4>
-                            <h5>$24.99</h5>
-                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                        </div>
-                        <div className="card-footer">
-                            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-lg-4 col-md-6 mb-4">
-                    <div className="card h-100">
-                        <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-                        <div className="card-body">
-                            <h4 className="card-title">
-                                <a href="#">Item Four</a>
-                            </h4>
-                            <h5>$24.99</h5>
-                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                        </div>
-                        <div className="card-footer">
-                            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-lg-4 col-md-6 mb-4">
-                    <div className="card h-100">
-                        <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-                        <div className="card-body">
-                            <h4 className="card-title">
-                                <a href="#">Item Five</a>
-                            </h4>
-                            <h5>$24.99</h5>
-                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div className="card-footer">
-                            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-lg-4 col-md-6 mb-4">
-                    <div className="card h-100">
-                        <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-                        <div className="card-body">
-                            <h4 className="card-title">
-                                <a href="#">Item Six</a>
-                            </h4>
-                            <h5>$24.99</h5>
-                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                        </div>
-                        <div className="card-footer">
-                            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                        </div>
-                    </div>
-                </div>
+                    )
+                })}
             </>
         );
     }
 }
 
 export default Movielist;
+
+// &#9733; &#9733; &#9733; &#9733; &#9734;
--- components/Sidemenu.js
+++ components/Sidemenu.js
@@ -1,7 +1,9 @@
-const Sidemenu = () => {
+
+const Sidemenu = (props) => {
+
     return (
         <div>
-            <h1 className="my-4">Shop Name</h1>
+            <h1>{props.appName}</h1>
             <div className="list-group">
               <a href="#" className="list-group-item">Category 1</a>
               <a href="#" className="list-group-item">Category 2</a>
